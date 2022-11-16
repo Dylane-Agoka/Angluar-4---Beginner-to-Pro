e40e7b8716afeeb93e13ebc316a13b4a6dcad69d
= <=>
2022-11-16 04:20:19 +0100

    attribute binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,28 +1,19 @@
-import { CourseComponent } from './course/course.component';
 import { Component } from "@angular/core";
-import { CoursesService } from "./courses.service";
 
 @Component({
   selector: 'courses',
   styleUrls: ['./courses.component.scss'],
-  // string interpolation : {{ item }}
-  // property biding : [attr] = "item"
+  // attribute binding : [attr.customPropertyName] = "item"
   template: `
-    <h2>{{ title }}</h2>
-    <ul>
-      <li *ngFor="let course of courses">{{ course }}</li>
-    </ul>
-    <h3 [textContent]="title"></h3>
-    <img [src]="imageUrl" alt="Some code using angular">
+    <img [src]="" alt="">
+    <table>
+      <tr>
+        <td [attr.colspan]="colSpan"></td>
+      </tr>
+    </table>
   `
-  // We use directive to manipulate the DOM
 })
 export class CoursesComponent {
-  title = 'List of courses';
-  courses;
-  imageUrl = './assets/images/pexels-markus-spiske.jpg';
-
-  constructor(service: CoursesService) {
-    this.courses = service.getCourses();
-  }
+  imageUrl = '';
+  colSpan = 2;
 }
