dda6e766b6ce7d02640eeb3a5d1f2fb67edbcbcf
= <=>
2022-11-16 05:15:59 +0100

    Style Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,11 +3,10 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'courses',
   styleUrls: ['./courses.component.scss'],
-  // add bootstrap: npm i bootstrap@version --save or npm i bootstrap --save
   template: `
-    <button class="btn btn-primary" [class.active]="isActive">Save</button>
+    <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>
   `
 })
 export class CoursesComponent {
-  isActive = true;
+  isActive = false;
 }
