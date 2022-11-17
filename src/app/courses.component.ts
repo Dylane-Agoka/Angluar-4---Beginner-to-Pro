bdb91691624d6047341d4eca63892cc376527421
= <=>
2022-11-17 02:58:17 +0100

    Pipe

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,14 +2,20 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'courses',
-  // two-way Binding : [(formModuleProperty)] = "templateVariable"
   template: `
-    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()">
+    {{ course.title | uppercase | lowercase }} <br>
+    {{ course.students | number }} <br>
+    {{ course.rating | number:'1.2-3' }} <br>
+    {{ course.price | currency:'CAD':true:'3.2-2' }} <br>
+    {{ course.releaseDate | date:'shortDate' }}
   `
 })
 export class CoursesComponent {
-  email = 'me@example.com';
-  onKeyUp() {
-    console.log(this.email);
+  course = {
+    title: 'The complete Angular Course',
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1)
   }
 }
