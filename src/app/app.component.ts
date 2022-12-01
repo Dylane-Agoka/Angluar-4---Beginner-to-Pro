fc3455d706d25f349f1373a84f2a0603d4d98806
= <=>
2022-12-01 06:46:49 +0100

    ngSwitchCase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  courses = ['sport', 'computer science'];
+  viewMode = 'something else';
 }
