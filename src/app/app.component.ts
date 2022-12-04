62ea369d453ef62f288b35b11f56a12f07101260
= <=>
2022-12-04 11:54:17 +0100

    ngStyle v1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  courses;
-
-  loadCourses() {
-    this.courses = [
-      { id: 1, name: 'Computer science' },
-      { id: 1, name: 'English language' },
-      { id: 1, name: 'Manual work' }
-    ];
-  }
-  onRemove(course) {
-    let index = this.courses.indexOf(course);
-    this.courses.splice(index, 1);
-  }
+  canSave = false;
 }
