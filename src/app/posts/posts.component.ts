ec7cf360e847b039554b15fe4beb7ff05a6b5777
Dylane-Agoka <dylaneagoka47@gmail.com>
2022-12-08 11:20:53 +0100

    Handling Expected Errors | Deprecated

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -13,49 +13,59 @@ export class PostsComponent implements OnInit{
 
   ngOnInit(): void {
     this.postService.getPosts()
-      .subscribe(response => {
-        this.posts = response;
-      },
-      error => {
-        alert('An unexpected error occurred.');
-        console.log(error);
-      });
+      .subscribe(
+        response => {
+          this.posts = response;
+        },
+        error => {
+          alert('An unexpected error occurred.');
+          console.log(error);
+        });
   }
 
   createPost(input: HTMLInputElement) {
     let post = { title: input.value};
     input.value = '';
     this.postService.createPost(post)
-      .subscribe(() => {
-        this.posts.splice(0, 0, post);
-      },
-      error => {
-        alert('An unexpected error occurred.');
-        console.log(error);
-      });
+      .subscribe(
+        () => {
+          this.posts.splice(0, 0, post);
+        },
+        error => {
+          alert('An unexpected error occurred.');
+          console.log(error);
+        });
   }
 
   updatePost(post) {
     this.postService.updatePost(post)
-      .subscribe(response => {
-        console.log(response);
-      },
-      error => {
-        alert('An unexpected error occurred.');
-        console.log(error);
-      });
+      .subscribe(
+        response => {
+          console.log(response);
+        },
+        error => {
+          alert('An unexpected error occurred.');
+          console.log(error);
+        });
   }
 
   deletePost(post) {
-    this.postService.deletePost(post.id)
-      .subscribe(() => {
-        let index = this.posts.indexOf(post);
-        this.posts.splice(index, 1);
-        console.log(`${post.title} was deleted with successful.`);
-      },
-      error => {
-        alert('An unexpected error occurred.');
-        console.log(error);
-      });
+    this.postService.deletePost(345)
+      .subscribe(
+        () => {
+          let index = this.posts.indexOf(post);
+          this.posts.splice(index, 1);
+          console.log(`${post.title} was deleted with successful.`);
+        },
+        (error: Response) => {
+          if (error.status === 404) {
+            alert('This post has already been deleted.');
+          } else {
+            alert('This post has already been deleted.');
+            console.log(error);
+          }
+          alert('An unexpected error occurred.');
+          console.log(error);
+        });
   }
 }
